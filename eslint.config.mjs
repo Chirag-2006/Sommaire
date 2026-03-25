c72945fe0f4af67b216d205068f584436196ba91
Chirag Arya <carya6785@gmail.com>
2026-03-26 03:08:55 +0530

    Enhance ESLint configuration by integrating Prettier support and defining custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +24,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  ...compat.extends(prettier),
+  ...compat.config({
+    rules: {
+      "prettier/prettier": "error",
+      "react/no-escape-entities": "off",
+    },
+    plugins: {
+      prettier: prettier,
+    },
+  }),
 ]);
 
 export default eslintConfig;
