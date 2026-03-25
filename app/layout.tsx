448892d1d9cbb130070eb5e2f8afc5523c78f922
Chirag Arya <carya6785@gmail.com>
2026-03-26 02:14:44 +0530

    Add ShadCn Ui and test button componet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Source_Sans_3 as FontSans } from "next/font/google";
+import { Source_Sans_3 as FontSans, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 const fontSans = FontSans({
   variable: "--font-sans  ",
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
-
 export const metadata: Metadata = {
   title: "Sommaire - Ai-Powered PDF Summarization",
   description:
@@ -22,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${fontSans.variable} font-sans h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable, fontSans.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { UploadIcon } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex justify-center items-center flex-col">
     <h1 className="text-4xl font-bold">Hello World</h1>
+    <Button size={"default"}>
+      <UploadIcon className="w-4 h-4" />
+      Upload PDF
+    </Button>
     </div>
   );
 }
